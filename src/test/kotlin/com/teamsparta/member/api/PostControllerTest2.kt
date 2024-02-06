1a535143928b47cf83b578f688bc5e517681c46f
Miso <un5754@naver.com>
2024-02-06 19:25:16 +0900

    feat: PostControllerTest2 테스트 추가

---
+++ src/test/kotlin/com/teamsparta/member/api/PostControllerTest2.kt
@@ -0,0 +1,37 @@
+package com.teamsparta.member.api
+
+import com.teamsparta.member.application.PostService
+import com.teamsparta.member.dto.res.PostResponse
+import io.kotest.core.spec.style.BehaviorSpec
+import io.kotest.matchers.shouldBe
+import io.mockk.every
+import io.mockk.mockk
+import org.springframework.http.HttpStatus
+
+class PostControllerTest2: BehaviorSpec ({
+    val savedPostId = 1L
+    val notSavedPostId = 10L
+    // 주입 뫀
+    val postService = mockk<PostService>()
+    // 테스트 할 레이어
+    val postController = PostController(postService)
+
+    val savedPost = PostResponse(savedPostId,"title", "content")
+    every { postService.getPostById(savedPostId) } returns savedPost
+
+    Given("a saved post Id") {
+        val targetPostId = savedPostId
+
+        When("execute getPost method") {
+            val result = postController.getPost(targetPostId)
+
+
+            Then("status code should be ok") {
+                result.statusCode shouldBe HttpStatus.OK
+                result.body?.id shouldBe savedPost.id       // ResponseEntity body 의 아이디가 저장된 post 의 id 여야 한다.
+            }
+        }
+    }
+
+
+})
