a80ef15baac4456846ef3de0e9d21f5dffd7efd2
Miso <un5754@naver.com>
2024-02-06 20:39:45 +0900

    feat: JPA Auditing 작성

--- src/main/kotlin/com/teamsparta/member/dto/req/PostRequest.kt
+++ src/main/kotlin/com/teamsparta/member/dto/req/PostRequest.kt
@@ -1,7 +1,5 @@
 package com.teamsparta.member.dto.req
 
-import java.time.LocalDateTime
-
 data class PostRequest(
     val title: String,
     val content: String,
--- src/main/kotlin/com/teamsparta/member/dto/req/SignUpRequest.kt
+++ src/main/kotlin/com/teamsparta/member/dto/req/SignUpRequest.kt
@@ -1,12 +1,6 @@
 package com.teamsparta.member.dto.req
 
-import com.teamsparta.member.domain.Member
 import com.teamsparta.member.dto.UserRole
-import jakarta.validation.constraints.Email
-import jakarta.validation.constraints.NotNull
-import jakarta.validation.constraints.Pattern
-import jakarta.validation.constraints.Size
-import org.springframework.security.crypto.password.PasswordEncoder
 
 
 data class SignUpRequest(
